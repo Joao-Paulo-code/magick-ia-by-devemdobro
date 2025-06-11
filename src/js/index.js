754dd5530d96902529c094ea3bf512a2a86da4da
Joao-Paulo-code <paulojoao1312@gmail.com>
2025-06-10 22:10:54 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
 /*
-    O que precisamos fazer? - Quando o usuário clicar no botão "Aplicar filtros", vamos filtrar as cartas baseado na categoria e no preço máximo selecionados
-    OBJETIVO 1 - Criar a funcionalidade de filtrar as cartas
+    O que precisamos fazer? - Quando o usuário clicar no botão "Aplicar filtros", vamos filtrar as cartas baseado na categoria e no preço máximo selecionados.
+    OBJETIVO: Criar a funcionalidade de filtrar as cartas
 */
 
-// passo 1 - pegar o botao de aplicar filtros  do HTML e mandar pro JS
+// passo 1 - pegar o botao de aplicar filtros do HTML e mandar pro JS
 const botaoFiltrar = document.querySelector('.btn-filtrar');
 
 // passo 2 - escutar o clique no botão de aplicar filtros
-botaoFiltrar.addEventListener('click', function () {
-    // passo 3 - pegar os valores dos campos de categoria e preço
+botaoFiltrar.addEventListener('click', aplicarFiltros);
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        aplicarFiltros();
+    }
+});
+function aplicarFiltros() {
+// passo 3 - pegar os valores dos campos de categoria e preço
     const categoriaSelecionada = document.querySelector('#categoria').value;
     const precoMaximoSelecionado = document.querySelector('#preco').value;
-    
+    console.log('categoriaselecionada', categoriaSelecionada);
     // passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida baseado nos filtros que a pessoa digitou
     const cartas = document.querySelectorAll(".carta");
 
@@ -19,16 +25,15 @@ botaoFiltrar.addEventListener('click', function () {
         const categoriaCarta = carta.dataset.categoria;
         const precoCarta = carta.dataset.preco;
         let mostrarCarta = true;
-
+        // Verificação de Filtragem de Categoria
         const temFiltroDeCategoria = categoriaSelecionada !== "";
 
-        const cartaNaoBateComFiltroDeCategoria =
-            categoriaSelecionada.toLowerCase() !== categoriaCarta.toLowerCase();
+        const cartaNaoBateComFiltroDeCategoria = categoriaSelecionada.toLowerCase() !== categoriaCarta.toLowerCase();
 
         if (temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria) {
             mostrarCarta = false;
         }
-
+        // Verificação de Filtragem de preço
         const temFiltroDePreco = precoMaximoSelecionado !== "";
 
         const cartaNaoBateComFiltroDePrecoMaximo =
@@ -45,8 +50,8 @@ botaoFiltrar.addEventListener('click', function () {
             carta.classList.remove("mostrar");
             carta.classList.add("esconder");
         }
-
     });
-});
+}
+
 
 
